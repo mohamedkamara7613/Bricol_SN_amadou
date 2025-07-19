fb70b0cd00bd1553915ff1b431c016bddb0666a6
mohamedkamara7613 <mohamedkamara7613@gmail.com>
2025-07-19 07:20:32 +0200

    Remove unnecessary whitespace in RegisterStep1 component

--- frontend/src/pages/RegisterStep1.jsx
+++ frontend/src/pages/RegisterStep1.jsx
@@ -106,7 +106,6 @@ export default function RegisterStep1() {
                 onChange={handleChange}
                 className="w-full border px-4 py-2 rounded focus:outline-none focus:ring-2 focus:ring-blue-500"
                 required
-                
             />
             )}
 
